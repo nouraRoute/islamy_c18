dced0c55064ecb280b0c74568ac8c9a502b92e80
Abdalla-Eldaly <abdalla90eldaly90@gmail.com>
2026-03-08 22:00:07 +0200

    create quran list and search feature

--- lib/tabs/quran_tab/quran_tab_page.dart
+++ lib/tabs/quran_tab/quran_tab_page.dart
@@ -4,13 +4,43 @@ import 'package:flutter_svg/svg.dart';
 import 'package:islamy/common/app_colors.dart';
 import 'package:islamy/common/widgets/bg_layout_widget.dart';
 import 'package:islamy/common/widgets/header_widget.dart';
+import 'package:islamy/data/models/sura_model.dart';
+import 'package:islamy/data/quran_data.dart';
 import 'package:islamy/gen/assets.gen.dart';
 import 'package:islamy/tabs/quran_tab/widgets/custom_text_field.dart';
 import 'package:islamy/tabs/quran_tab/widgets/most_recent_section.dart';
 import 'package:islamy/tabs/quran_tab/widgets/suras_list_section.dart';
 
-class QuranTabPage extends StatelessWidget {
+class QuranTabPage extends StatefulWidget {
   const QuranTabPage({super.key});
+
+  @override
+  State<QuranTabPage> createState() => _QuranTabPageState();
+}
+
+class _QuranTabPageState extends State<QuranTabPage> {
+  final TextEditingController controller = TextEditingController();
+  List<SuraModel> _filteredSuras = QuranData.suras;
+
+  void _onSearchChanged(String searchText) {
+ setState(() {
+   if (searchText.isEmpty) {
+     _filteredSuras = QuranData.suras;
+   } else {
+     _filteredSuras = QuranData.suras.where(
+           (sura) {
+         final query = searchText.toLowerCase();
+         return sura.enName.toLowerCase().contains(searchText) ||
+             sura.arName.toLowerCase().contains(searchText) ||
+             sura.number.toString() == searchText;
+       },
+     ).toList();
+   }
+ });
+  }
+
+  // a
+  //A
 
   @override
   Widget build(BuildContext context) {
@@ -20,26 +50,30 @@ class QuranTabPage extends StatelessWidget {
         SafeArea(
           child: Padding(
             padding: const EdgeInsets.only(top: 30, left: 20, right: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                HeaderWidget(),
-                SizedBox(height: 20),
-                CustomTextField(
-                  hintText: "Sura Name",
-                  prefixIcon: Padding(
-                    padding: const EdgeInsets.all(14.0),
-                    child: SvgPicture.asset(
-                      Assets.quranTabIcon,
-                      colorFilter: ColorFilter.mode(
-                        AppColors.goldColor,
-                        BlendMode.srcIn,
+            child: CustomScrollView(
+              slivers: [
+                SliverToBoxAdapter(child: HeaderWidget()),
+                SliverToBoxAdapter(child: SizedBox(height: 20)),
+                SliverToBoxAdapter(
+                  child: CustomTextField(
+                    onChanged:  _onSearchChanged ,
+                    // onSubmitted: _onSearchChanged,
+                    controller: controller,
+                    hintText: "Sura Name",
+                    prefixIcon: Padding(
+                      padding: const EdgeInsets.all(14.0),
+                      child: SvgPicture.asset(
+                        Assets.quranTabIcon,
+                        colorFilter: ColorFilter.mode(
+                          AppColors.goldColor,
+                          BlendMode.srcIn,
+                        ),
                       ),
                     ),
                   ),
                 ),
-                MostRecentSection(),
-                SurasListSection(),
+                SliverToBoxAdapter(child: MostRecentSection()),
+                SurasListSection(suras: _filteredSuras,),
               ],
             ),
           ),
